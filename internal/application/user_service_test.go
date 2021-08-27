007a5ed5296c7f9deb73a81db1540e2938076559
Lucasdox <lucas.augusto29@gmail.com>
2021-08-27 01:33:34 -0300

    adding tests

--- internal/application/user_service_test.go
+++ internal/application/user_service_test.go
@@ -1,13 +1,17 @@
 package application
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/gofrs/uuid"
+
 	"game-project/internal/application/command"
 	"game-project/internal/application/query"
 	"game-project/internal/domain"
-	"github.com/gofrs/uuid"
-	"reflect"
-	"testing"
 )
 
 func TestUserServiceImpl_CreateUser(t *testing.T) {
@@ -79,6 +83,107 @@ func TestUserServiceImpl_ListUser(t *testing.T) {
 		if !reflect.DeepEqual(result, tc.expectedResult) {
 			t.Errorf("expected result: %v , actual: %v", tc.expectedResult, result)
 		}
+	}
+}
+
+func TestUserServiceImpl_ListUserFriends(t *testing.T) {
+	cases := []struct {
+		fakeRepository *fakeUserRepository
+		expectedResult *query.UserFriends
+		expectedErr error
+	}{
+		{
+			fakeRepository: &fakeUserRepository{
+				listFriendsMock: []*domain.User{
+					{
+						Id:   uuid.UUID{},
+						Name: "Jessica",
+						GamesPlayed: sql.NullInt32{
+							Int32: 1,
+							Valid: true,
+						},
+						Score: sql.NullInt64{
+							Int64: 200,
+							Valid: true,
+						},
+					},
+					{
+						Id:          uuid.UUID{},
+						Name:        "Paul",
+					},
+				},
+				findUserMock: &domain.User{
+					Id:          uuid.UUID{},
+					Name:        "Don",
+				},
+			},
+			expectedResult: &query.UserFriends{
+				Friends: []*query.Friend{
+					{
+						Id:        uuid.UUID{},
+						Name:      "Jessica",
+						Highscore: 200,
+					},
+					{
+						Id:        uuid.UUID{},
+						Name:      "Paul",
+						Highscore: 0,
+					},
+				},
+			},
+			expectedErr:    nil,
+		},
+		{
+			fakeRepository: &fakeUserRepository{
+				listFriendsMock: []*domain.User{
+					{
+						Id:   uuid.UUID{},
+						Name: "Jessica",
+						GamesPlayed: sql.NullInt32{
+							Int32: 1,
+							Valid: true,
+						},
+						Score: sql.NullInt64{
+							Int64: 200,
+							Valid: true,
+						},
+					},
+					{
+						Id:          uuid.UUID{},
+						Name:        "Paul",
+					},
+				},
+				findUserMock: nil,
+			},
+			expectedResult: nil,
+			expectedErr:    errors.New(fmt.Sprintf("no user with id %s found", uuid.UUID{})),
+		},
+	}
+
+	for _, tc := range cases {
+		service := UserServiceImpl{repository: tc.fakeRepository}
+		response, err := service.ListUserFriends(uuid.UUID{})
+
+		if tc.expectedErr != nil {
+			if err.Error() != tc.expectedErr.Error() {
+				t.Errorf("expected err %s, actual: %s", tc.expectedErr, err)
+			}
+		} else {
+			if err != tc.expectedErr {
+				t.Errorf("expected err %s, actual: %s", tc.expectedErr, err)
+			}
+		}
+
+		if tc.expectedResult != nil {
+			if !reflect.DeepEqual(response, tc.expectedResult){
+				t.Errorf("expected respone to be %v, actual: %v", tc.expectedResult, response)
+			}
+		} else {
+			if response != nil {
+				t.Error("result should be nil")
+			}
+		}
+
 	}
 }
 
